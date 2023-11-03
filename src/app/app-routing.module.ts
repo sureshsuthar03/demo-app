6188cfb83357f93b8cc01987405bdf65f171c7dd
sureshsuthar03 <sureshsuthar57695@gmail.com>
2023-11-03 11:06:59 +0530

    try

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
       { path: "country-upsert/:id", component: CountryUpsertComponent },
     ],
   },
-  { path: "*", redirectTo: "home" },
+  { path: "*", redirectTo: "home/city" },
 ];
 
 @NgModule({
